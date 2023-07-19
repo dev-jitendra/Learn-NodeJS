b1147038488407fa4c770dd7340d72a501560d48
Jitendra Lodhi <jitendra.developer77@gmail.com>
2023-07-19 18:47:16 +0530

    Your commit message here

---
+++ url-module.mjs
@@ -0,0 +1,14 @@
+// URL MODULE
+import {URL} from 'url'
+const myURL=new URL('https://chat.openai.com/c/d?5242775-f9d6-4c61-9992-2751065e426f#jitendra-?lodhi-lsdf');
+console.log(myURL.hash);
+console.log(myURL.host);
+console.log(myURL.hostname);
+console.log(myURL.href);
+console.log(myURL.pathname);
+console.log(myURL.port);
+console.log(myURL.protocol);
+console.log(myURL.search);
+console.log(myURL.searchParams);
+console.log(myURL.toString());
+console.log(myURL.toJSON());
